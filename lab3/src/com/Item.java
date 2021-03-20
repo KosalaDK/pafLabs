c6629f9cdc0a70f62eb2dc689863173f668f821c
“KosalaDK” <kosaladisanayake.dk@gmail.com>
2021-03-20 22:24:30 +0530

    Did some correction item.java

--- lab3/src/com/Item.java
+++ lab3/src/com/Item.java
@@ -10,8 +10,7 @@ public class Item {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf2021", "root", "kosala");
-			// For testing
-			System.out.print("Successfully connected");
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
